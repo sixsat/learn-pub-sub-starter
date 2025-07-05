a8578addd647be68635721428ddcb73e7a11e5eb
sat <77387934+sixsat@users.noreply.github.com>
2025-07-05 21:59:19 +0700

    init server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitURL = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	log.Println("Peril game server connected to RabbitMQ!")
+
+	pubCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not open a channel: %v", err)
+	}
+
+	_, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.GameLogSlug,
+		routing.GameLogSlug+".*",
+		pubsub.QTypeDurable,
+	)
+	if err != nil {
+		log.Fatalf("could not subscribe to pause: %v", err)
+	}
+	log.Printf("Queue %s declared and bound!\n", queue.Name)
+	gamelogic.PrintServerHelp()
+
+	// server REPL (read-eval-print loop)
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "pause":
+			log.Println("publishing pause game state")
+			publishJSON(pubCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+		case "resume":
+			log.Println("publishing resume game state")
+			publishJSON(pubCh, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+		case "quit":
+			log.Println("quitting server")
+			return
+		default:
+			log.Printf("unknown command: %s", words[0])
+		}
+	}
+}
+
+func publishJSON(ch *amqp.Channel, exchange string, key string, val routing.PlayingState) {
+	err := pubsub.PublishJSON(ch, exchange, key, val)
+	if err != nil {
+		log.Printf("could not publish message: %v\n", err)
+	}
 }
